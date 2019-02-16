946e8e838e6624043b62dcf2c74dc5a67d03bfe5
hellosirandy <sac840711@gmail.com>
2019-02-16 13:55:01 -0500

    finish modal

--- src/screens/CreateStoryScreen/index.js
+++ src/screens/CreateStoryScreen/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 
 class CreateStoryScreen extends React.PureComponent {
   render() {
     return (
-      <div>test</div>
+      <View>
+        <Text>
+          Hello
+        </Text>
+      </View>
     );
   }
 }
