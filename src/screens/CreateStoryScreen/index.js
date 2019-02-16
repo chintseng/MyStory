4d32aba00225593fe75abcbfd8bb5b46416ebb57
Chin <chin@10-18-5-134.dynapool.wireless.nyu.edu>
2019-02-16 14:18:50 -0500

    working on the screen

--- src/screens/CreateStoryScreen/index.js
+++ src/screens/CreateStoryScreen/index.js
@@ -1,16 +1,58 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import styles from './styles';
+import { RNCamera } from 'react-native-camera';
+import { Icon } from 'react-native-elements';
 
 class CreateStoryScreen extends React.PureComponent {
+  static navigationOptions = ({ navigation }) => {
+    // const onCameraClick = navigation.getParam('onCameraClick');
+    return {
+      title: 'Camera',
+      headerStyle: {
+        backgroundColor: '#fca17d',
+      },
+      headerRight: (<Icon
+        name="camera-alt"
+        // onPress={() => {
+        //   navigation.navigate('CreateStoryScreen');
+        // }}
+        underlayColor="transparent"
+      />),
+    };
+  };
   render() {
     return (
-      <View>
-        <Text>
-          Hello
-        </Text>
+      <View style={styles.container}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          permissionDialogTitle={'Permission to use camera'}
+          permissionDialogMessage={'We need your permission to use your camera phone'}
+          onGoogleVisionBarcodesDetected={({ barcodes }) => {
+            console.log(barcodes);
+          }}
+        />
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
+
+  takePicture = async function() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
 }
 
 export default CreateStoryScreen;
